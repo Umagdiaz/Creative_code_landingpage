bf215ed4793366dbdae22eeb906992f280c43e61
uma granado diaz <umabaligranado@gmail.com>
2025-12-15 00:39:23 -0300

    reestructuracion

--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,11 @@
 let emptyElement = document.querySelector(".about .empty");
-let titleElement = document.querySelector(".about .title")
-
-
+let titleElement = document.querySelector(".about .title");
 
 window.addEventListener("mousemove", handleMouseMove);
 
 function handleMouseMove(event) { 
     emptyElement.style.flexBasis = event.clientX + "px";
-    titleElement.style.flexBasis = event.clientY/2 + "px"; 
+    titleElement.style.flexBasis = event.clientY + "px"; 
 }
 
 
