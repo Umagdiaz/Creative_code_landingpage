297d1d39bac2698fb0594e2e12bfe337f33b6295
uma granado diaz <umabaligranado@gmail.com>
2025-12-16 01:09:59 -0300

    reunificacion js

--- Js/script.js
+++ Js/script.js
@@ -22,4 +22,41 @@ btnMenu.addEventListener('click', () => {
     btnMenu.setAttribute('aria-expanded', isOpen)
 });
 
+/*footer*/
+let textureCounter = 0;
+const maxTextureValue = 140; // Define un máximo para textureCounter
+let canvas;
+
+function setup() {
+ let canvasContainer = document.getElementById('canvasContainer');
+    canvas = createCanvas(canvasContainer.offsetWidth, canvasContainer.offsetHeight);
+    canvas.parent('canvasContainer');
+    frameRate(60);
+}
+
+function draw() {  
+  generateTexture();
+}
+
+function generateTexture() {
+  loadPixels();
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let index = (x + y * width) * 4;
+      // Calcula brightnessValue basado en noise y textureCounter
+      let brightnessValue = noise(x * 0.01, y * 0.01) * random(50, 80 + textureCounter);
+      pixels[index] = brightnessValue;
+      pixels[index + 1] = brightnessValue;
+      pixels[index + 2] = brightnessValue;
+      pixels[index + 3] = 245;
+    }
+  }
+  updatePixels();
+  textureCounter += 5;
+
+  if (textureCounter >= maxTextureValue) {
+    textureCounter = 0; 
+  }
+}
+
 
